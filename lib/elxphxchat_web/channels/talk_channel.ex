021da7a96834f8da476f78c4a833ac51c676eadf
gentom <gento.m.developer@gmail.com>
2017-12-31 07:04:09 +0900

    commit

--- lib/elxphxchat_web/channels/talk_channel.ex
+++ lib/elxphxchat_web/channels/talk_channel.ex
@@ -1,12 +1,12 @@
 defmodule ElxphxchatWeb.TalkChannel do
     use Phoenix.Channel
-  
+
     def join("talk", _payload, socket) do
-      {:ok, socket}
+        {:ok, socket}
     end
-  
+
     def handle_in("new_message", payload, socket) do
-      broadcast! socket, "new_message", payload
-      {:noreply, socket}
+        broadcast! socket, "new_message", payload
+        {:noreply, socket}
     end
-  end
+end
